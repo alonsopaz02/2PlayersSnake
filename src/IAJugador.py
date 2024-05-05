7f422009c2cfb2c43932259f3917fb873c105865
alonsopaz02 <jose.paz9@unmsm.edu.pe>
2024-05-05 00:25:04 -0500

    ia easy mode

--- src/IAJugador.py
+++ src/IAJugador.py
@@ -1,7 +1,50 @@
+import random
+from cabeza import Cabeza
+
 class IAJugador:
-    def __init__(self,dificultad):
+    def __init__(self,dificultad,cabezaX,cabezaY):
         self.dificultad = dificultad
         self.color = "A"
+        self.cabezaX = cabezaX
+        self.cabezaY = cabezaY
         
-    def elegir_posicion(self):
         
+    def set_cabezaX(self,x):
+        self.cabezaX = x
+        
+    def set_cabezaY(self,y):
+        self.cabezaY = y
+        
+    def get_cabezaX(self):
+        return self.cabezaX
+        
+    def get_cabezaY(self):
+        return self.cabezaY
+    
+    
+    def elegir_posicion_facil(self,cabezaX,cabezaY):
+        vecinos = []
+        a = cabezaX
+        b = cabezaY
+        puntos_vecinos = [(a+1, b), (a-1, b), (a, b+1), (a, b-1)]
+
+        # Procesar cada punto vecino
+        for punto in puntos_vecinos:
+            x, y = punto  # Coordenadas del punto vecino
+
+            # Verificar si el valor es 8 o 0 y aplicar las modificaciones
+            if x == 8:
+                x = 1
+            elif x == 0:
+                x = 7
+
+            if y == 8:
+                y = 1
+            elif y == 0:
+                y = 7
+
+            # Agregar el punto modificado al arreglo de vecinos
+            vecinos.append((x, y))
+            
+        numero_random = random.randint(0, 3)
+        return vecinos[numero_random]
---
+++ src/mainIA.py
@@ -0,0 +1,124 @@
+from juego import Juego
+from IAJugador import IAJugador
+import pygame
+import sys
+import time
+
+
+def draw_matrix(T):
+    for row in range(ROWS):
+        for col in range(COLS):
+            value = T[row][col]
+
+            if isinstance(value, int):
+                # Dibujar número
+                font = pygame.font.Font(None, 36)
+                text_surface = font.render(str(value), True, WHITE)
+                text_rect = text_surface.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                window.blit(text_surface, text_rect)
+            elif value == 'R':
+                # Dibujar círculo rojo oscuro
+                pygame.draw.circle(window, DARK_RED, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), SQUARE_SIZE // 3)
+            elif value == 'r':
+                # Dibujar círculo rojo
+                pygame.draw.circle(window, RED, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), SQUARE_SIZE // 3)
+            elif value == 'A':
+                # Dibujar círculo azul oscuro
+                pygame.draw.circle(window, DARK_BLUE, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), SQUARE_SIZE // 3)
+            elif value == 'a':
+                # Dibujar círculo azul
+                pygame.draw.circle(window, BLUE, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), SQUARE_SIZE // 3)
+            elif value.isdigit():
+                font = pygame.font.Font(None, 36)
+                text_surface = font.render(value, True, WHITE)
+                text_rect = text_surface.get_rect(center=(col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2))
+                window.blit(text_surface, text_rect)
+
+# Inicializar Pygame
+pygame.init()
+
+# Dimensiones de la ventana y del tablero
+WIDTH = 800
+HEIGHT = 800
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = WIDTH // COLS
+
+# Definir colores
+BLACK = (0, 0, 0)
+DARK_RED = (139, 0, 0)
+RED = (255, 0, 0)
+DARK_BLUE = (0, 0, 139)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+# Crear la ventana
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Snake vs Snake')
+
+# Función principal
+def main():
+    juego = Juego("R")
+    ia = IAJugador("facil",999,999)
+    
+    running = True
+
+    ganador = "0"
+
+    while running:
+        for event in pygame.event.get():
+            if(juego.movimientos>1):
+                if(juego.verificarEstadoMeta(juego.estado.get_x_cabezaRoja(),juego.estado.get_y_cabezaRoja())):
+                    ganador = "Azul"
+                    break
+                if(juego.verificarEstadoMeta(juego.estado.get_x_cabezaAzul(),juego.estado.get_y_cabezaAzul())):
+                    ganador = "Rojo"
+                    break
+
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                a = y // SQUARE_SIZE
+                b = x // SQUARE_SIZE
+
+                if(juego.movimientos<=1):
+                    if(juego.estado.get_turno() == "R"):
+                        if(juego.establecerPrimeraFichaRoja(a,b)==False):
+                            break
+                        else:
+                            juego.movimientos = juego.movimientos + 1
+                    elif(juego.estado.get_turno()== "A"):
+                        
+                        if(juego.establecerPrimeraFichaAzul(a,b)==False):
+                            break
+                        else:
+                            juego.movimientos = juego.movimientos + 1
+                else:
+                    if(juego.estado.get_turno()== "R"):
+                        if(juego.establecerRojo(a,b)==False):
+                            break
+                    else:
+                        posicion=ia.elegir_posicion_facil(juego.estado.get_x_cabezaAzul(),juego.estado.get_y_cabezaAzul())
+                        a = posicion[0]
+                        b = posicion[1]
+                        print(a)
+                        print(b) 
+                        if(juego.establecerAzul(a,b)==False):
+                            break
+        window.fill(BLACK)
+        draw_matrix(juego.estado.get_tablero().get_casillas())
+        print("Turno de " + juego.estado.get_turno())
+        pygame.display.flip()
+
+        if(ganador != "0"):
+            print("Gana "+ ganador)
+            pygame.display.flip()
+            time.sleep(10)
+            break
+
+if __name__ == '__main__':
+    main()
